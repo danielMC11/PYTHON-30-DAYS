59dc8750894352c5f00f8af8c89ac16ce407732d
Daniel MC <danielcarrillo3200@gmail.com>
2023-03-19 19:11:40 -0500

    Reg Ex

--- Regular Expresions.py
+++ Regular Expresions.py
@@ -29,6 +29,7 @@ list_sep.clear()
 list_sep = re.split(' ', matches_replaced_string)
 print(list_sep)
 
+
 txt = '''Hello1 and hello2'''
 
 #Square Bracket
@@ -67,5 +68,114 @@ reg_ex = r'2023$'   # ends string with pattern
 print(re.findall(reg_ex, txt))
 
 # Quantifiers Store in a slot of the list the pattern extended if substring matches
-txt = ''
-reg_ex = r''
+txt = 'Shadow_Spartan777'
+reg_ex = r'S*'  # pattern length can be 0 or more
+print(re.findall(reg_ex, txt))
+reg_ex = r'[A-Za-z]+'  # pattern length can be 1 or more
+print(re.findall(reg_ex, txt))
+reg_ex = r'[_7]?' # pattern length can be 0 or 1
+print(re.findall(reg_ex, txt))
+reg_ex = r'[A-Za-z_\d]{3}'  # pattern length is exactly 3
+print(re.findall(reg_ex, txt))
+reg_ex = r'[A-Za-z_\d]{1,3}' # pattern length is between 1 and 3
+print(re.findall(reg_ex, txt))
+
+paragraph = 'I love teaching. If you do not love teaching what else can you love.I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
+rx_words = r'[^ .]+'
+words = re.findall(rx_words, paragraph)
+print(words)
+
+frequent_words = [(len(re.findall(r'[ .]?' + i + r'[ .]', paragraph)), i) for i in set(words)]
+print(frequent_words)
+
+
+def is_valid_variable(var):
+    valid = r'[a-z]+_?[a-z]+'
+    match = re.match(valid, var)
+    if match:
+        start, end = match.span()
+        print(var == var[start:end])
+    else:
+        print(False)
+
+
+is_valid_variable('first_name')
+is_valid_variable('first-name')
+is_valid_variable('1first_name')
+is_valid_variable('firstname')
+
+
+def extract_numbers(txt):
+    pattern = r'-?\d+'
+    list_numbers = [int(i) for i in re.findall(pattern, txt)]
+    return list_numbers
+
+
+statement = '''The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction.
+Extract these numbers from this whole text and find the distance between the two furthest particles.'''
+
+
+numbers = extract_numbers(statement)
+numbers.sort()
+max_distance = abs(numbers[len(numbers)-1] - numbers[0])
+print(max_distance)
+
+def clean_text(txt):
+    pattern = r'[^a-zA-z\s.!?]'
+    new_str = re.sub(pattern, '', txt)
+    return new_str
+
+def get_words_txt(txt):
+    pattern = r'[A-Za-z]+'
+    list_words = re.findall(pattern, txt)
+    return list_words
+
+def unique_words(txt):
+    list_uniq = list()
+    for i in txt:
+        if i not in list_uniq:
+            list_uniq.append(i)
+    return list_uniq
+
+
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+
+new_sentence = clean_text(sentence)
+print(new_sentence)
+words = get_words_txt(new_sentence)
+u_words = unique_words(words)
+
+re_clean = r'[^a-zA-z ]+'
+
+f_words = [(i, len(re.findall(r' ' + i + r' ', ' ' + re.sub(re_clean, '', new_sentence) + ' '))) for i in u_words]
+print(f_words)
+
+
+list_indexes = list()
+list_sorted = list()
+var = 0
+
+
+def bubble_sort_l(list_):
+    for i in range(len(list_)):
+        for j in range(0, len(list_) - i - 1):
+            if list_[j] > list_[j + 1]:
+                temp = list_[j]
+                list_[j] = list_[j + 1]
+                list_[j + 1] = temp
+
+
+def bubble_sort_lt(list_tup):
+    for i in range(len(list_tup)):
+        for j in range(0, len(list_tup) - i - 1):
+            if list_tup[j][1] < list_tup[j + 1][1]:
+                temp = list_tup[j]
+                list_tup[j] = list_tup[j + 1]
+                list_tup[j + 1] = temp
+
+
+bubble_sort_lt(f_words)
+print(f_words)
+
+
+
