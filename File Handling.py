24f531f6e5d6b5e5431ac197a5c1867076986a7e
Daniel MC <danielcarrillo3200@gmail.com>
2023-03-20 23:02:20 -0500

    File Handling, exercises left

--- File Handling.py
+++ File Handling.py
@@ -1,7 +1,7 @@
 """Reading"""
 """
                                     #  r and t defaulf value
-f = open('my_file.txt', mode='rt')  # r (read) a (append) w (write) x (create) t (text mode) b (binary mode)
+f = open('my_file.txt', mode='rt')# r (read) a (append) w (write) x (create) t (text mode) b (binary mode)
 txt = f.read()                      # return str all text
 #print(txt)
 f.close()
@@ -20,8 +20,8 @@ f.close()
 with open('my_file.txt') as f:
   txt = f.read().splitlines()  # method same as read lines - No \n included
   print(txt)
-"""
 
+"""
 """
 #with open('my_file.txt', mode='a') as f:           # Appending to the existing text
     f.write('\nI am currently studying system engineering.')
@@ -66,7 +66,7 @@ import csv
 with open('text_csv.csv', mode='r') as f:
     csv_reader = csv.reader(f, delimiter=',')
     lines_count = 0
-
+ 
     for row in csv_reader:
         if lines_count == 0:
             print(f"Keys: {', '.join(row)}")
@@ -76,7 +76,89 @@ with open('text_csv.csv', mode='r') as f:
             lines_count += 1
     print(f'Number of lines: {lines_count}')
 """
+# XML
+"""
+xml_str = '''<?xml version="1.0"?>
+<data>
+    <country name="Liechtenstein">
+        <rank>1</rank>
+        <year>2008</year>
+        <gdppc wealth="rich">141100</gdppc>
+        <neighbor name="Austria" direction="E"/>
+        <neighbor name="Switzerland" direction="W"/>
+    </country>
+    <country name="Singapore">
+        <rank>4</rank>
+        <year>2011</year>
+        <gdppc wealth="rich">59900</gdppc>
+        <neighbor name="Malaysia" direction="N"/>
+    </country>
+    <country name="Panama">
+        <rank>68</rank>
+        <year>2011</year>
+        <gdppc wealth="poor">13600</gdppc>
+        <neighbor name="Costa Rica" direction="W"/>
+        <neighbor name="Colombia" direction="E"/>
+    </country>
+</data>'''
+root = ET.fromstring(xml_str)
+"""
 
+"""
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('Countries.xml')
+root = tree.getroot()
+
+for country in root.findall('country'):
+    name = country.get('name')
+    gdp = country.find('gdppc').text
+    gdp_type = country.find('gdppc').get('wealth')
+    print(f'Country: {name}, Gdp: {gdp} and Type of Wealth: {gdp_type}')
+
+for neighbor_tag in root.iter('neighbor'):
+    print(neighbor_tag.attrib)
+
+for year_tag in root.iter('year'):
+    print(year_tag.tag, year_tag.text)
+
+for country in root.findall('country'):
+    if country.get('name') == 'Liechtenstein':
+        country.set('continent', 'europe')
+    elif country.get('name') == 'Singapore':
+        country.set('continent', 'asia')
+    else:
+        country.set('continent', 'south-america')
+
+
+#root.append(ET.SubElement(root, 'Country', {'name': 'Colombia', 'continent': 'south-america'}))
+
+tree.write('Countries.xml')
+"""
+
+# Exercises
+"""
+import re
+def count_lines_and_words(document):
+    pattern = r'[A-Za-z]+'
+    list_words = list()
+    for line in document:
+        list_words.extend(re.findall(pattern, line))
+    print(f'Number of lines: {len(document)}, Number of words: {len(list_words)}')
+
+
+with open('obama_speech.txt', mode='r') as file:
+    count_lines_and_words(file.readlines())
+"""
+
+import json
+
+"""
+try:
+    data = json.load('countries_data.json')
+except Exception as e:
+    print(type(e), e)
+"""
 
 
 
